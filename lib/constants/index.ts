4e43f0661eb733a3d6c9e3d83c23260a262ca29d
ndutagrace25 <ndutagrace25@gmail.com>
2025-02-07 11:42:38 +0300

    project setup

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,1 +1,3 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Online Store";
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "A modern ecommerce store"
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "HTTP://localhost:3000"
