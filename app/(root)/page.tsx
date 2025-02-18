34019d8f3558f5441fa3171421f4fd188033377e
ndutagrace25 <ndutagrace25@gmail.com>
2025-02-18 11:26:39 +0300

    added sample data and product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,15 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 const HomePage = () => {
-  return <>Online Store</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrival"
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default HomePage;
